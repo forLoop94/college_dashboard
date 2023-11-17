739fd564e9910dbb0ee847cb93ea0452b8c7fe1f
forLoop94 <charlesuwem94@gmail.com>
2023-11-17 16:35:06 +0100

    Fetch actual lecturer data, update store, render on lecturers page

--- src/redux/lecturer/lecturerSlice.js
+++ src/redux/lecturer/lecturerSlice.js
@@ -1,27 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const baseURL = "http://localhost:4000/api/v1";
+const token = localStorage.getItem('token');
+
+export const getLecturers = createAsyncThunk("lecturers/getLecturers", async() => {
+  try {
+    const response = await fetch(`${baseURL}/lecturers`, {
+      method: "GET",
+      authorization: `${token}`
+    });
+    if(response.ok) {
+      const lecturerData = await response.json();
+      console.log(lecturerData);
+      return lecturerData;
+    }
+  } catch(error) {
+    throw new error(error.message);
+  }
+})
 
 const initialState = {
-  lecturers: [
-    {
-      id: 1,
-      name: "Lect Prince",
-      class: 23
-    },
-    {
-      id: 2,
-      name: "Lect Prince 2",
-      class: 28
-    }
-  ],
+  lecturers: [],
 }
 
 const lecturerSlice = createSlice({
   name: "lecturers",
   initialState,
   reducers: {},
-  // extraReducers: (builder) => {
-  //   builder.addCase()
-  // }
+  extraReducers: (builder) => {
+    builder.addCase(getLecturers.fulfilled, (state, action) => {
+      state.lecturers = action.payload;
+    })
+  }
 })
 
 export default lecturerSlice.reducer
