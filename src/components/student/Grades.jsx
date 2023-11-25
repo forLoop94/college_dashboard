0bc68404be5064d855276b2edda474a9c5de71a9
forLoop94 <charlesuwem94@gmail.com>
2023-11-25 01:40:28 +0100

    Add grade point avg calculator to calculate student GPA in real time when lecturer adds score

--- src/components/student/Grades.jsx
+++ src/components/student/Grades.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCourseGrades } from '../../redux/grade/gradeSlice';
 import { gradeAlphabet } from '../../utils/gradeAlphabet';
+import { gradePoint } from '../../utils/gradePoint';
 
 export const Grades = () => {
   const dispatch = useDispatch();
@@ -11,17 +12,27 @@ export const Grades = () => {
     dispatch(getCourseGrades());
   }, [])
 
+  const gradePointCalculator = () => {
+    const totalQualityPoints = gradesInfo.reduce((total, num) => total + (gradePoint(gradeAlphabet(num.grade)) * num.credit_load), 0);
+    const totalCredit = gradesInfo.reduce((total, num) => total + num.credit_load, 0);
+    const gpa = totalQualityPoints / totalCredit;
+    return gpa.toFixed(2);
+  }
+
   return (
    <section>
     <h1>Course and Grade Information</h1>
     {gradesInfo.map((course) => (
       <article key={course.id}>
-        <div>{course.title}</div>
-        <div>{course.code}</div>
-        <div>{course.grade}</div>
-        <div>{gradeAlphabet(course.grade)}</div>
+        <div>Title: {course.title}</div>
+        <div>Code: {course.code}</div>
+        <div>Credit Hours: {course.credit_load}</div>
+        <div>Score: {course.grade}</div>
+        <div>Symbol: {gradeAlphabet(course.grade)}</div>
+        <div>Point: {gradePoint(gradeAlphabet(course.grade))}</div>
       </article>
     ))}
+    <h2>GPA: {gradePointCalculator()}</h2>
    </section>
   )
 }
