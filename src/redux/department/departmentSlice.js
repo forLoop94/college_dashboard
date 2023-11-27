24df878bfd9d98c0ab4d26cd316e9bfb40ebbd4e
forLoop94 <charlesuwem94@gmail.com>
2023-11-27 16:12:57 +0100

    Write thunks that fetch students, lecturers and courses from an Hod's dept and stores in the redux store

--- src/redux/department/departmentSlice.js
+++ src/redux/department/departmentSlice.js
@@ -30,8 +30,50 @@ export const getDepartments = createAsyncThunk('department/getDepartments', asyn
   }
 })
 
+export const getDepartmentStudents = createAsyncThunk('department/getDepartmentStudents', async() => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${baseURL}/department_students`, {
+    headers: {
+      authorization: token
+    }
+  })
+  if(response.ok) {
+    const data = await response.json()
+    return data;
+  }
+})
+
+export const getDepartmentLecturers = createAsyncThunk('department/getDepartmentLecturers', async() => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${baseURL}/department_lecturers`, {
+    headers: {
+      authorization: token
+    }
+  })
+  if(response.ok) {
+    const data = await response.json()
+    return data;
+  }
+})
+
+export const getDepartmentCourses = createAsyncThunk('department/getDepartmentCourses', async() => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${baseURL}/department_courses`, {
+    headers: {
+      authorization: token
+    }
+  })
+  if(response.ok) {
+    const data = await response.json()
+    return data;
+  }
+})
+
 const initialState = {
-  departments: []
+  departments: [],
+  departmentStudents: [],
+  departmentLecturers: [],
+  departmentCourses: [],
 }
 
 const departmentSlice = createSlice({
@@ -40,6 +82,15 @@ const departmentSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(getDepartments.fulfilled, (state, action) => {
       state.departments = action.payload
+    })
+    builder.addCase(getDepartmentCourses.fulfilled, (state, action) => {
+      state.departmentCourses = action.payload
+    })
+    builder.addCase(getDepartmentLecturers.fulfilled, (state, action) => {
+      state.departmentLecturers = action.payload
+    })
+    builder.addCase(getDepartmentStudents.fulfilled, (state, action) => {
+      state.departmentStudents = action.payload
     })
   }
 })
