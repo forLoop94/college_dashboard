d54fa58b3e59fdf457acb22152d4673f00e65b2d
forLoop94 <charlesuwem94@gmail.com>
2023-11-28 15:09:46 +0100

    Add form to AllotmentData.jsx to collect HOD input and post it to the db

--- src/components/hod/AllotmentData.jsx
+++ src/components/hod/AllotmentData.jsx
@@ -1,48 +1,99 @@
-import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllotmentData } from "../../redux/allotment_data/allotmentSlice";
+import { addAllotmentData, getAllotmentData } from "../../redux/allotment_data/allotmentSlice";
+import { getDepartmentCourses, getDepartmentLecturers } from "../../redux/department/departmentSlice";
 
 export const AllotmentData = () => {
   const dispatch = useDispatch();
   const allotmentInfo = useSelector((state) => state.Allotments.allotmentData);
   const { profile_id } = useSelector((state) => state.user.currentUser);
+  const courses = useSelector((state) => state.Departments.departmentCourses);
+  const lecturers = useSelector(
+    (state) => state.Departments.departmentLecturers
+  );
+  const [data, setData] = useState({
+    lecturer_id: '',
+    course_id: ''
+  })
 
   useEffect(() => {
     dispatch(getAllotmentData(profile_id));
+    dispatch(getDepartmentCourses());
+    dispatch(getDepartmentLecturers());
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addAllotmentData(data));
+    dispatch(getAllotmentData(profile_id));
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    })
+  }
+
   return (
-    <div>
-      {allotmentInfo.length === 0 ? (
-        <p>No allotments</p>
-      ) : (
-        <div>
-          <h2>Course Allotment Table</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Lecturers</th>
-                <th>Courses</th>
-                <th>Manage allotments</th>
-              </tr>
-            </thead>
-            <tbody>
-              {allotmentInfo.map((row) => (
-                <tr key={row.id}>
-                  <td>
-                    {row.lecturer.first_name} {row.lecturer.last_name}
-                  </td>
-                  <td>{row.course.title}</td>
-                  <td>
-                    <button>Cancel</button>
-                  </td>
+    <section>
+      <h2>Course Allotment Form</h2>
+      <form onSubmit={handleSubmit}>
+        <select
+          value={data.lecturer_id}
+          name="lecturer_id"
+          onChange={handleChange}
+        >
+          <option value="">Lecturers</option>
+          {lecturers.map((lecturer) => (
+            <option key={lecturer.id} value={lecturer.id}>{lecturer.first_name} {lecturer.last_name}</option>
+          ))}
+        </select>
+        <select
+          value={data.course_id}
+          name="course_id"
+          onChange={handleChange}
+        >
+          <option value="">Courses</option>
+          {courses.map((course) => (
+            <option key={course.id} value={course.id}>{course.title}</option>
+          ))}
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {allotmentInfo.length === 0 ? (
+          <p>No allotments</p>
+        ) : (
+          <div>
+            <h2>Course Allotment Table</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Lecturers</th>
+                  <th>Courses</th>
+                  <th>Manage allotments</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-    </div>
+              </thead>
+              <tbody>
+                {allotmentInfo.map((row) => (
+                  <tr key={row.id}>
+                    <td>
+                      {row.lecturer.first_name} {row.lecturer.last_name}
+                    </td>
+                    <td>{row.course.title}</td>
+                    <td>
+                      <button>Cancel</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
--- src/redux/allotment_data/allotmentSlice.js
+++ src/redux/allotment_data/allotmentSlice.js
@@ -2,44 +2,49 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const baseURL = "http://localhost:4000/api/v1";
 
-export const addAllotmentData = createAsyncThunk('lecturerCourses/addAllotmentData', async(data) => {
-  const token = localStorage.getItem('token');
-  const response = await fetch(`${baseURL}/lecturer_courses`, {
-    method: 'POST',
-    header: {
-      "Content-Type": "application/json",
-      authorization: token
-    },
-    body: JSON.stringify(data)
-  })
-  const result = await response.json();
-  return result;
-})
+export const addAllotmentData = createAsyncThunk(
+  "lecturerCourses/addAllotmentData",
+  async (data) => {
+    const token = localStorage.getItem("token");
+    const response = await fetch(`${baseURL}/lecturer_courses`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    return result;
+  }
+);
 
-export const getAllotmentData = createAsyncThunk('lecturerCourses/getAllotmentData', async(id) => {
-  const token = localStorage.getItem('token');
-  const response = await fetch(`${baseURL}/lecturer_courses/${id}`, {
-    header: {
-      authorization: token
-    }
-  })
-  const data = await response.json()
-  return data;
-})
+export const getAllotmentData = createAsyncThunk(
+  "lecturerCourses/getAllotmentData",
+  async (id) => {
+    const token = localStorage.getItem("token");
+    const response = await fetch(`${baseURL}/lecturer_courses/${id}`, {
+      header: {
+        authorization: token,
+      },
+    });
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-  allotmentData: []
-}
+  allotmentData: [],
+};
 
 const allotmentSlice = createSlice({
-  name: 'allotmentData',
+  name: "allotmentData",
   initialState,
   extraReducers: (builder) => {
     builder.addCase(getAllotmentData.fulfilled, (state, action) => {
       state.allotmentData = action.payload;
-    })
-  }
-})
+    });
+  },
+});
 
 export default allotmentSlice.reducer;
-
