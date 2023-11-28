03005437474cb3e4d9e43cf36f712d98b8b64b31
forLoop94 <charlesuwem94@gmail.com>
2023-11-28 11:52:10 +0100

    Add thunks to save and retrieve lecturer-course allotment information from the db

---
+++ src/components/hod/AllotmentData.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AllotmentData = () => {
+  return (
+    <div>AllotmentData</div>
+  )
+}
---
+++ src/redux/allotment_data/allotmentSlice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const baseURL = "http://localhost:4000/api/v1";
+
+export const addAllotmentData = createAsyncThunk('lecturerCourses/addAllotmentData', async(data) => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${baseURL}/lecturer_courses`, {
+    method: 'POST',
+    header: {
+      "Content-Type": "application/json",
+      authorization: token
+    },
+    body: JSON.stringify(data)
+  })
+  const result = await response.json();
+  return result;
+})
+
+export const getAllotmentData = createAsyncThunk('lecturerCourses/getAllotmentData', async() => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${baseURL}/lecturer_courses`, {
+    header: {
+      authorization: token
+    }
+  })
+  const data = await response.json()
+  return data;
+})
+
+const initialState = {
+  allotmentData: []
+}
+
+const allotmentSlice = createSlice({
+  name: 'allotmentData',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getAllotmentData.fulfilled, (state, action) => {
+      state.allotmentData = action.payload;
+    })
+  }
+})
+
+export default allotmentSlice.reducer;
+
