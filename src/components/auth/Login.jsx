a155c413c183e2dcb9180606776594bcad5653c8
forLoop94 <charlesuwem94@gmail.com>
2023-12-04 14:03:24 +0100

    Style log in page

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,65 +1,79 @@
 // import React from 'react'
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../../styles/auth_pages.css";
+import { FaGraduationCap } from "react-icons/fa";
 
 export const Login = () => {
   const navigate = useNavigate();
   const [data, setData] = useState({
-    'email': '',
-    'password': ''
-  })
+    email: "",
+    password: "",
+  });
 
   const baseURL = "http://localhost:4000";
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await fetch(`${baseURL}/login`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          user: data
-        })
-      })
-      if(response.ok) {
-        const resData = await response.json()
+          user: data,
+        }),
+      });
+      if (response.ok) {
+        const resData = await response.json();
         console.log(resData);
-        const authorization = response.headers.get('authorization');
-        localStorage.setItem('token', authorization);
+        const authorization = response.headers.get("authorization");
+        localStorage.setItem("token", authorization);
         console.log(authorization);
         console.log(resData.status.message);
         navigate("/");
       }
-    } catch(error) {
+    } catch (error) {
       return error;
     }
-  }
+  };
 
   const handleChange = (e) => {
     setData({
       ...data,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='email'
-        placeholder='email'
-        name='email'
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        placeholder='password'
-        name='password'
-        onChange={handleChange}
-      />
-      <button type='submit'>Submit</button>
-      <Link to='/signup'>Sign up</Link>
-    </form>
-  )
-}
+    <section className="vh-100 vw-100 d-flex justify-content-center align-items-center auth-forms">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          className="form-control"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <div class="d-flex justify-content-center align-items-center">
+          <button class="btn btn-primary">
+            Log in
+            <FaGraduationCap className="ms-2"/>
+          </button>
+        </div>
+        <div className="text-white position-absolute top-0 end-0 m-5">
+          Not registered?
+          <Link to="/signup">Sign up</Link>
+        </div>
+      </form>
+    </section>
+  );
+};
