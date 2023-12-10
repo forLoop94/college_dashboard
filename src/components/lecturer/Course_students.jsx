1782916eba5cfaf6a9db211f1551ca508934a183
forLoop94 <charlesuwem94@gmail.com>
2023-12-10 17:49:39 +0100

    Modify CourseStudents component to include submissions button to render Dialogue component

--- src/components/lecturer/Course_students.jsx
+++ src/components/lecturer/Course_students.jsx
@@ -1,28 +1,52 @@
-import { useState } from 'react';
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCourseStudents } from '../../redux/course/courseSlice';
-import { Grade } from './Grade';
-import PropTypes from 'prop-types';
-import { Button, Modal } from 'react-bootstrap';
-import { propTypes } from 'react-bootstrap/esm/Image';
+import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCourseStudents } from "../../redux/course/courseSlice";
+import { Grade } from "./Grade";
+import PropTypes from "prop-types";
+import { Button, Modal } from "react-bootstrap";
+import { propTypes } from "react-bootstrap/esm/Image";
+import { LessonArea } from "../student/lesson_area/Lesson-area";
 
 export const CourseStudents = ({ course, showAssignedCourses }) => {
   const dispatch = useDispatch();
   const [selectedStudentId, setSelectedStudentId] = useState(null);
+  const [studentDetails, setStudentDetails] = useState({
+    id: null,
+    first_name: "",
+    last_name: "",
+  });
   const students = useSelector((state) => state.Courses.students);
 
   useEffect(() => {
     dispatch(getCourseStudents(course.id));
-  }, [dispatch, course.id])
+  }, [dispatch, course.id]);
 
   const createGrade = (studentId) => {
     setSelectedStudentId(studentId);
-  }
+  };
 
   const closeGradeModal = () => {
     setSelectedStudentId(null);
   };
+
+  const fetchStudent = (id, fName, lName) => {
+    setStudentDetails({
+      id: id,
+      first_name: fName,
+      last_name: lName,
+    });
+  };
+
+  const showStudents = (bool) => {
+    if(bool) {
+      setStudentDetails({
+        id: null,
+        first_name: '',
+        last_name: ''
+      })
+    }
+  }
 
   return (
     <section>
@@ -36,10 +60,28 @@ export const CourseStudents = ({ course, showAssignedCourses }) => {
           <div>{student.photo}</div>
           <div>{student.gender}</div>
           <div>{student.level}</div>
-          <Button variant='primary' onClick={() => createGrade(student.id)}>Grade</Button>
+          <button
+            className="btn btn-dark me-5"
+            onClick={() =>
+              fetchStudent(student.id, student.first_name, student.last_name)
+            }
+          >
+            Submissions & Dialogue
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={() => createGrade(student.id)}
+          >
+            Grade
+          </button>
         </article>
       ))}
-      <button className='btn btn-primary mt-5' onClick={() => showAssignedCourses(true)}>Back to assigned courses</button>
+      <button
+        className="btn btn-primary mt-5"
+        onClick={() => showAssignedCourses(true)}
+      >
+        Back to assigned courses
+      </button>
       {selectedStudentId && (
         <Modal show={true} onHide={closeGradeModal}>
           <Modal.Body>
@@ -51,13 +93,16 @@ export const CourseStudents = ({ course, showAssignedCourses }) => {
           </Modal.Body>
         </Modal>
       )}
+      <div>
+        {studentDetails.id && <LessonArea key={studentDetails.id} studentDetails={studentDetails} courseInfo={course} showStudents={showStudents} />}
+      </div>
     </section>
-  )
-}
+  );
+};
 
 CourseStudents.propTypes = {
   course: PropTypes.shape({
     id: PropTypes.number,
-    title: propTypes.string
-  })
-}
+    title: PropTypes.string,
+  }),
+};
