1b394b682430b09ec1f2056f150ae93159061333
forLoop94 <charlesuwem94@gmail.com>
2023-12-22 14:17:33 +0100

    Style components rendering courses in current_user HOD department

--- src/components/hod/DepartmentCourses.jsx
+++ src/components/hod/DepartmentCourses.jsx
@@ -1,31 +1,39 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { getDepartmentCourses } from '../../redux/department/departmentSlice';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDepartmentCourses } from "../../redux/department/departmentSlice";
 
 export const DepartmentCourses = () => {
   const dispatch = useDispatch();
   const courses = useSelector((state) => state.Departments.departmentCourses);
 
   useEffect(() => {
-    dispatch(getDepartmentCourses())
-  }, [dispatch])
+    dispatch(getDepartmentCourses());
+  }, [dispatch]);
 
   if (courses.length === 0) {
-    return "No courses yet!"
+    return "No courses yet!";
   } else {
     return (
-      <section>
-      <h1>Courses in {courses[0].department.name} department</h1>
-      {courses.map((course) => (
-        <article key={course.id}>
-          <h3>Course title: {course.title}</h3>
-          <div>Course code: {course.code}</div>
-          <div>Level offering: {course.level}</div>
-          <div>Credit Hours: {course.credit_load}</div>
-          <div>Department domiciled: {course.department.name}</div>
-        </article>
-      ))}
-    </section>
-    )
+      <section className="technical-pages-bg">
+        <h1 className="tech-header">Courses</h1>
+        <div className="d-flex flex-column align-items-center tech-card-container">
+          <small className="small-note-light">
+            Courses in {courses[0].department.name} department
+          </small>
+          {courses.map((course) => (
+            <article
+              className="tech-card-v3 d-flex mb-5 justify-content-between align-items-center"
+              key={course.id}
+            >
+              <h5>{course.title}</h5>
+              <div className="small-fonts">Code: {course.code}</div>
+              <div className="small-fonts">Level: {course.level}</div>
+              <div className="small-fonts">Credit Hours: {course.credit_load}</div>
+              <div className="small-fonts">Department: {course.department.name}</div>
+            </article>
+          ))}
+        </div>
+      </section>
+    );
   }
-}
+};
