d8d3761949487293c059f9172526be376f9a650b
forLoop94 <charlesuwem94@gmail.com>
2024-01-01 21:23:50 +0100

    Fix stylelint errors

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,74 +1,41 @@
-import { useState } from "react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import { getCurrentUser } from "../redux/user/userSlice";
 import { NavPanel } from "./NavPanel";
 
 export const Root = () => {
-  const [userData, setUserData] = useState({
-    role: "",
-    profile_exists: "",
-  });
+  const { role, profile_exists } = useSelector((state) => state.user.currentUser);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
 
-  const baseURL = "http://localhost:4000";
-
-  const getUser = async () => {
-    const token = localStorage.getItem("token");
-
-    const response = await fetch(`${baseURL}/current_user`, {
-      headers: {
-        authorization: `${token}`,
-      },
-    });
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else if(response.status === 401) {
-      navigate("/login")
-    }
-  };
-
   useEffect(() => {
-    dispatch(getCurrentUser());
     const fetchData = async () => {
-      const data = await getUser();
-
-      if(!data) {
-        navigate("/login");
-        return;
-      } else {
-        setUserData({
-          role: data.role,
-          profile_exists: data.profile_exists
-        })
-      }
+      dispatch(getCurrentUser());
 
       if (!token) {
         navigate("/login");
         return;
       }
 
-      if (!userData.profile_exists && !userData.role) {
+      if (!profile_exists && !role) {
         return;
       }
 
-      const addProfilePath = `/add_${userData.role.toLowerCase()}`;
+      const addProfilePath = `/add_${role.toLowerCase()}`;
 
-      if (!userData.profile_exists) {
+      if (!profile_exists) {
         navigate(addProfilePath);
         return;
       }
 
-      if (userData.profile_exists && userData.role === "student") {
+      if (profile_exists && role === "student") {
         navigate("/student_details");
-      } else if (userData.profile_exists && userData.role === "lecturer") {
+      } else if (profile_exists && role === "lecturer") {
         navigate("/lecturer_details");
-      } else if (userData.profile_exists && userData.role === "hod") {
+      } else if (profile_exists && role === "hod") {
         navigate("/hod_details");
       } else {
         navigate("/dean_details");
@@ -76,7 +43,7 @@ export const Root = () => {
     };
 
     fetchData();
-  }, [token, userData.profile_exists, userData.role, navigate, dispatch]);
+  }, [token, profile_exists, role, navigate, dispatch]);
 
   return (
     <div id="root">
