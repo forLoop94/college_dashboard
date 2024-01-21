122bd4fadc7e1ef3dc10bc303d18c69ec2bb2537
forLoop94 <charlesuwem94@gmail.com>
2024-01-21 11:44:21 +0100

    Fix bg-color problem in chats page and allignment in submissions

--- src/components/student/lesson_area/Submissions.jsx
+++ src/components/student/lesson_area/Submissions.jsx
@@ -1,7 +1,20 @@
-export const Submissions = () => {
+import { FaAngleLeft } from "react-icons/fa"
+
+export const Submissions = ({ setLinkPages }) => {
   return (
-    <div className="technical-pages-bg-v2 d-flex justify-content-center align-items-center">
+    <div className="technical-pages-bg-v2 d-flex justify-content-center align-items-center p-5">
       <h1 className="text-white">Feature available in next release!</h1>
+      <button
+        className="inner-page-return"
+        onClick={() =>
+          setLinkPages({
+            submission: false,
+            chats: false,
+          })
+        }
+      >
+        <FaAngleLeft />
+      </button>
     </div>
   )
 }
